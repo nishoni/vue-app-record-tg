bf31183d06cac25f74a540e8b1786eb4e4fc1434
Maria Smorodina <mariya.smorodina@insales.ru>
2023-08-01 23:23:14 +0300

    now at least it's working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import Vue from 'vue'
+import MyComponent from '../components/MyComponent.vue'
+
+Vue.component('my-component', MyComponent)
