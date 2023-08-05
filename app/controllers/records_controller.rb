53a8736b1d38e21412bd83679c8923e8dab06c85
Maria Smorodina <mariya.smorodina@insales.ru>
2023-08-05 22:33:52 +0300

    настройка главных контроллеров бэка

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,3 +1,9 @@
 class RecordsController < ApplicationController
-  def index; end
+  def index
+    @records = Record.all
+  end
+
+  def show
+    @record = Record.find_by(id: params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_193051) do
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "login"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
